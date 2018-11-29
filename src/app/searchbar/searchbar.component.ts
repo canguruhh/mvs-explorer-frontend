24e9776817cf0214cf96c7f8fb6c912c7a7277f4
Laurent Salou <laurent.salou@hotmail.fr>
2018-11-29 18:04:09 +0800

    Update searchbar suggestions

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -4,6 +4,16 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { ExplorerService } from './../services/explorer/explorer.service';
 import { Suggestions } from './../models/suggestion.model';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
+import { FormControl } from '@angular/forms';
+import { catchError, map, tap,startWith, switchMap, debounceTime, distinctUntilChanged, takeWhile, first } from 'rxjs/operators';
+
+
+export interface State {
+  flag: string;
+  name: string;
+  population: string;
+}
 
 @Component({
   selector: 'app-searchbar',
@@ -12,57 +22,221 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class SearchbarComponent implements OnInit {
 
-  options: string[] = ['One', 'Two', 'Three'];
+  suggestionsCtrl = new FormControl();
+  initialSuggestions: any = [];
+  filteredSuggestions: Observable<any>;
+
+  suggestions: any[] = []
 
   constructor(private explorer : ExplorerService, public translate: TranslateService) {
     explorer.getSuggestions('lau', 10).subscribe((response: Suggestions)=>{
       console.log(response)
     })
+
+    this.filteredSuggestions = this.suggestionsCtrl.valueChanges
+    .pipe(
+          startWith(null),
+          debounceTime(500),
+          distinctUntilChanged(),
+          switchMap(query => {
+            console.log(query)
+            return (query && query.length >= 3) ? this.search(query) : this.initialSuggestions
+          })
+        );
+    //  debounceTime(500),
+    //  .startWith(null)
+    //  .subscribe(query => (query && query.length >= 3) ? this.search(query) : this.initialSuggestions)
+
+
   }
 
   ngOnInit() {
+
+
+
+      /*.pipe(
+        startWith(''),
+        map(query => (query && query.length >= 3) ? this.search(query) : this.initialSuggestions)
+      );*/
+
+    this.translate.get(['SUGGESTION.SHOW_ALL_TX', 'SUGGESTION.SHOW_ALL_BLOCKS', 'SUGGESTION.SHOW_ALL_ASSETS', 'SUGGESTION.SHOW_ALL_AVATARS', 'SUGGESTION.SHOW_ALL_MITS']).subscribe(translations => {
+      this.initialSuggestions.push({
+        'name': translations['SUGGESTION.SHOW_ALL_TX'],
+        'url': 'txs',
+        'type': 'tx',
+      });
+      this.initialSuggestions.push({
+        'name': translations['SUGGESTION.SHOW_ALL_BLOCKS'],
+        'url': 'blocks',
+        'type': 'blockHeight',
+      });
+      this.initialSuggestions.push({
+        'name': translations['SUGGESTION.SHOW_ALL_ASSETS'],
+        'url': 'assets',
+        'type': 'asset',
+        'icon': 'default_mst'
+      });
+      this.initialSuggestions.push({
+        'name': translations['SUGGESTION.SHOW_ALL_AVATARS'],
+        'url': 'avatars',
+        'type': 'avatar',
+        'icon': 'default_avatar'
+      });
+      this.initialSuggestions.push({
+        'name': translations['SUGGESTION.SHOW_ALL_MITS'],
+        'url': 'mits',
+        'type': 'mit',
+        'icon': 'default_mit'
+      });
+    });
+
+    this.suggestions = this.initialSuggestions;
+
   }
+
+  private search(query: string) {
+    console.log(this.filteredSuggestions)
+    //this.explorer.getSuggestions(query, 10).toPromise().then((response)=>{
+    return this.explorer.getSuggestions(query, 10).toPromise().then((response)=>{
+      console.log(response)
+      if (response != undefined) {
+        return this.setResults(query, response);
+      } else return [];
+    })
+  }
+
+  private setResults(text, result) {
+      return Promise.all([this.setResultsInit(text), this.setResultsAsset(result.asset), this.setResultsAddress(result.address), this.setResultsAvatar(result.avatar), this.setResultsMit(result.mit), this.setResultsTx(result.tx), this.setResultsBlockHash(result.block)])
+          .then((results) => {
+              console.log(results.reduce((acc, val) => acc.concat(val)));
+              return results.reduce((acc, val) => acc.concat(val))
+            });
+  }
+
+  private setResultsInit(text) {
+      var repos = [];
+      if (!isNaN(text)) {
+          repos.push({
+              'name': text,
+              'url': 'blk/' + text,
+              'type': 'blockHeight',
+          });
+      } else if (text.toUpperCase() == "ETP") {
+          repos.push({
+              'name': 'ETP',
+              'url': 'asset/ETP',
+              'type': 'asset',
+              'icon': 'ETP'
+          });
+      }
+      return repos;
+  }
+
+  private setResultsAsset(assets) {
+      return assets.map((asset) => {
+          return {
+              name: asset,
+              url: "asset/" + asset,
+              type: "asset",
+              //icon: this.icons.indexOf(asset) > -1 ? asset : 'default_mst'
+          };
+      });
+  }
+
+  private setResultsAvatar(avatars) {
+      return avatars.map((avatar) => {
+          return {
+              name: avatar,
+              url: 'avatar/' + avatar,
+              type: 'avatar',
+              icon: 'default_avatar'
+          };
+      });
+  }
+
+  private setResultsAddress(addresses) {
+      return addresses.map((address) => {
+          return {
+              name: address.a,
+              url: "adr/" + address.a,
+              nbrtx: address.n,
+              type: "address"
+          };
+      });
+  }
+
+  private setResultsTx(txs) {
+      return txs.map((tx) => {
+          return {
+              name: tx.h,
+              url: "tx/" + tx.h,
+              height: tx.b,
+              type: "tx"
+          };
+      });
+  }
+
+  private setResultsBlockHash(blocks) {
+      return blocks.map((block) => {
+          return {
+              name: block.h,
+              url: "blk/" + block.h,
+              height: block.n,
+              type: "blockHash"
+          };
+      });
+  }
+
+  private setResultsMit(mits) {
+      return mits.map((mit) => {
+          return {
+              name: mit,
+              url: 'mit/' + mit,
+              type: 'mit',
+              icon: 'default_mit'
+          };
+
 
   /*
 
-  $scope.presEnterSearch = presEnterSearch;
+  this.presEnterSearch = presEnterSearch;
 
-  $scope.querySearch = querySearch;
-  $scope.selectedItemChange = selectedItemChange;
-  $scope.searchTextChange = searchTextChange;
-  $scope.search = search;
-  $scope.setResults = setResults;
-  $scope.setResultsTx = setResultsTx;
-  $scope.initialSuggestion = [];
-  $scope.init = init;
-  $scope.icons = Assets.hasIcon;
+  this.querySearch = querySearch;
+  this.selectedItemChange = selectedItemChange;
+  this.searchTextChange = searchTextChange;
+  this.search = search;
+  this.setResults = setResults;
+  this.setResultsTx = setResultsTx;
+  this.initialSuggestions = [];
+  this.init = init;
+  this.icons = Assets.hasIcon;
 
   function init() {
       $translate(['SUGGESTION.SHOW_ALL_TX', 'SUGGESTION.SHOW_ALL_BLOCKS', 'SUGGESTION.SHOW_ALL_ASSETS', 'SUGGESTION.SHOW_ALL_AVATARS', 'SUGGESTION.SHOW_ALL_MITS'])
           .then((translations) => {
-              $scope.initialSuggestion.push({
+              this.initialSuggestions.push({
                   'name': translations['SUGGESTION.SHOW_ALL_TX'],
                   'url': 'txs',
                   'type': 'tx',
               });
-              $scope.initialSuggestion.push({
+              this.initialSuggestions.push({
                   'name': translations['SUGGESTION.SHOW_ALL_BLOCKS'],
                   'url': 'blocks',
                   'type': 'blockHeight',
               });
-              $scope.initialSuggestion.push({
+              this.initialSuggestions.push({
                   'name': translations['SUGGESTION.SHOW_ALL_ASSETS'],
                   'url': 'assets',
                   'type': 'asset',
                   'icon': 'default_mst'
               });
-              $scope.initialSuggestion.push({
+              this.initialSuggestions.push({
                   'name': translations['SUGGESTION.SHOW_ALL_AVATARS'],
                   'url': 'avatars',
                   'type': 'avatar',
                   'icon': 'default_avatar'
               });
-              $scope.initialSuggestion.push({
+              this.initialSuggestions.push({
                   'name': translations['SUGGESTION.SHOW_ALL_MITS'],
                   'url': 'mits',
                   'type': 'mit',
@@ -74,7 +248,7 @@ export class SearchbarComponent implements OnInit {
   init();
 
   function querySearch(query) {
-      return (query && query.length >= 3) ? search(query) : $scope.initialSuggestion;
+      return (query && query.length >= 3) ? search(query) : this.initialSuggestions;
   }
 
 
@@ -229,7 +403,7 @@ export class SearchbarComponent implements OnInit {
               name: asset,
               url: "asset/" + asset,
               type: "asset",
-              icon: $scope.icons.indexOf(asset) > -1 ? asset : 'default_mst'
+              icon: this.icons.indexOf(asset) > -1 ? asset : 'default_mst'
           };
       });
   }
