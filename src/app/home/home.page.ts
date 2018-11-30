7d3c84056b08bf14520dd107991289757d5b6abf
Sven <sven@mutzl.org>
2018-11-30 08:35:20 +0100

    Cleanup home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { ExplorerService } from './../services/explorer/explorer.service';
-
-import { Tickers, Ticker, TickersList } from './../models/ticker.model'
-import { Avatar } from './../models/avatar.model'
 import { FormControl } from '@angular/forms';
 
 @Component({
@@ -14,13 +10,7 @@ export class HomePage {
 
   myControl = new FormControl();
 
-  constructor(private explorer : ExplorerService){
-    explorer.getTickers().subscribe((response: TickersList)=>{
-      console.log(response)
-    })
-    explorer.getAvatars().subscribe((response: Avatar[])=>{
-      console.log(response)
-    })
+  constructor(){
   }
 
 }
