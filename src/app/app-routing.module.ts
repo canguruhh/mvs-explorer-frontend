44795c37e2930552ca128ffe54939d3e9109f605
Laurent Salou <laurent.salou@hotmail.fr>
2018-11-30 15:06:20 +0800

    Add blank avatars and transactions pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'avatars', loadChildren: './avatars/avatars.module#AvatarsPageModule' },
+  { path: 'txs', loadChildren: './transactions/transactions.module#TransactionsPageModule' },
 ];
 
 @NgModule({
